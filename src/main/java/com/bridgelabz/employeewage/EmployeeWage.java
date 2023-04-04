7093a66902f116207d7f97141a48f58ed8a1980c
Mouli2000 <moulisitthan10@gmail.com>
2023-04-04 15:12:08 +0530

    Added : Add Part Time Employee and Wage

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -6,8 +6,11 @@ class EmoployeeWage {
 
 	static int WAGE_PER_HR = 20;
 	static int FULL_DAY_HR = 8;
+	static int PART_TIME_HR = 4;
 	static int PRESENT = 1;
 	static int DAILY_WAGE = 0;
+	static int FULL_TIME = 1;
+	static int PART_TIME = 0;
 	
 	public static void main(String []args) {
 		
@@ -16,8 +19,17 @@ class EmoployeeWage {
 		
 		if(randomNum == PRESENT) {
 			System.out.println("Employee Present");
-			DAILY_WAGE = WAGE_PER_HR * FULL_DAY_HR;
-			System.out.println("Daily Employee Wage is : "+DAILY_WAGE);
+			int check = random.nextInt(2);
+			if(check == FULL_TIME) {
+				DAILY_WAGE = WAGE_PER_HR * FULL_DAY_HR;
+				System.out.println("Employee is Full Timer");
+				System.out.println("Daily Employee Wage is : "+DAILY_WAGE);
+			}
+			else {
+				DAILY_WAGE = WAGE_PER_HR * PART_TIME_HR;
+				System.out.println("Employee is Part Timer");
+				System.out.println("Daily Employee Wage is : "+DAILY_WAGE);
+			}
 		}
 		else
 			System.out.println("Employee Absent");
